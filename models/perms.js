b8c6c1f1943d05b592521f42b3713c777955db50
TurtlePaw <turtlesimsyt@gmail.com>
2021-06-11 19:50:11 -0400

    ✅

--- models/perms.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const thanksSchema = new mongoose.Schema({
-    perm: Boolean,
-    user: String
-});
-
-const ThanksModel = module.exports = mongoose.model('perms', thanksSchema);
